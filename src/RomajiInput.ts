9b52fca13dee4f75aa7cfb8c2d4bf0272261244a
Yasuhiro Hayase <hayase@cs.tsukuba.ac.jp>
2023-10-29 13:12:10 +0900

    Fix n-related Romaji-input behavior

--- src/RomajiInput.ts
+++ src/RomajiInput.ts
@@ -1,14 +1,44 @@
 import * as vscode from 'vscode';
-import { romKanaBaseRule } from './RomKanaRule';
+import { RomKanaRule, romKanaBaseRule } from './RomKanaRule';
 import { insertOrReplaceSelection } from './extension';
 
 export class RomajiInput {
     private romBuffer: string[] = [];
 
     private static romToHiragana(str: string): string[] | undefined {
-        if (romKanaBaseRule[str]) {
-            return [romKanaBaseRule[str].hiragana, romKanaBaseRule[str].remain];
+        if (str.length === 0) {
+            return undefined;
         }
+
+        const rule = romKanaBaseRule[str];
+        if (rule) {
+            const count = RomajiInput.countPrefixOf(romKanaBaseRule, str);
+            if (count === 1) {
+                // Only one rule matches
+                return [rule.hiragana, rule.remain];
+            } else {
+                // Multiple rules with the same prefix match
+                
+                // Postpone the conversion until the next input
+                return undefined;
+            }
+        }
+        
+        // No exact match found.
+        // Try to find the longest prefix
+        for (let i = str.length; i > 0; i--) {
+            const prefix = str.slice(0, i);
+            const ruleForPrefix = romKanaBaseRule[prefix];
+            if (ruleForPrefix) {
+                // Match found for the prefix, convert the prefix and postpone the rest
+                return [ruleForPrefix.hiragana, ruleForPrefix.remain + str.slice(i)];
+            }
+        }
+        return undefined;
+    }
+
+    static countPrefixOf(romKanaBaseRule: { [key: string]: RomKanaRule }, str: string) : number {
+        return Object.keys(romKanaBaseRule).filter(key => key.startsWith(str)).length;
     }
 
     public processInput(key: string): void {
