c111993f0aa191ca256c90eefc01c68ead415e26
Yasuhiro Hayase <hayase@cs.tsukuba.ac.jp>
2024-02-10 17:42:30 +0900

    Save Henkan results so the resutls are used at the next VSCode launch.

--- src/jisyo/jisyo.ts
+++ src/jisyo/jisyo.ts
@@ -1,31 +1,61 @@
-import { assert } from "console";
 import { Uri } from "vscode";
 import * as vscode from 'vscode';
 import { Candidate } from "./candidate";
+import { CompositeMap } from "../lib/composite-map";
 
 type Jisyo = Map<string, Candidate[]>;
-type CompositeJisyo = CompositeMap<string, Candidate[]>;
 
-var userJisyo: Jisyo;
-var systemJisyo: Jisyo;
-var globalJisyo: CompositeJisyo;
+const userJisyoKey = "skk-vscode.user-jisyo";
+
+var globalJisyo: Jisyo;
 
 export async function init(memento: vscode.Memento): Promise<void> {
-    systemJisyo = await loadJisyoFromUri(memento, Uri.parse("https://raw.githubusercontent.com/skk-dev/dict/master/SKK-JISYO.L"));
-    userJisyo = new Map(); // await loadOrInitUserJisyo(memento);
-    globalJisyo = new CompositeMap([userJisyo, systemJisyo]);
+    let systemJisyo = await loadSystemJisyoFromUri(memento, Uri.parse("https://raw.githubusercontent.com/skk-dev/dict/master/SKK-JISYO.L"));
+    let userJisyo = loadOrInitUserJisyo(memento);
+    globalJisyo = new CompositeJisyo([userJisyo, systemJisyo], memento);
 }
 
-export function getGlobalJisyo(): CompositeJisyo {
+export function getGlobalJisyo(): Jisyo {
     return globalJisyo;
 }
 
-async function loadJisyoFromUri(memento: vscode.Memento, uri: Uri): Promise<Jisyo> {
-    const cacheKey = "skk-vscode.jisyo";
+class CompositeJisyo extends CompositeMap<string, Candidate[]> {
+    private memento: vscode.Memento;
+
+    constructor(jisyoList: Jisyo[], memento: vscode.Memento) {
+        super(jisyoList);
+        this.memento = memento;
+    }
+
+    set(key: string, value: Candidate[]): this {
+        super.set(key, value);
+        saveUserJisyo(this.memento, this.maps[0]);
+        return this;
+    }
+}
+
+function loadOrInitUserJisyo(memento: vscode.Memento): Jisyo {
+    // check if local cache is available
+    const cache = memento.get<Object>(userJisyoKey);
+    const now = Date.now();
+    if (cache) {
+        return new Map(Object.entries(cache));
+    }
+    
+    return new Map();
+}
+
+async function saveUserJisyo(memento: vscode.Memento, userJisyo: Jisyo): Promise<void> {
+    await memento.update(userJisyoKey, Object.fromEntries(userJisyo));
+}
+
+
+async function loadSystemJisyoFromUri(memento: vscode.Memento, uri: Uri): Promise<Jisyo> {
+    const systemJisyoKey = "skk-vscode.jisyo";
     const cacheExpiryKey = "skk-vscode.jisyo-expiry";
 
     // check if local cache is available
-    const cache = memento.get<Object>(cacheKey);
+    const cache = memento.get<Object>(systemJisyoKey);
     const cacheExpiry = memento.get<number>(cacheExpiryKey);
     const now = Date.now();
     if (cache && cacheExpiry && now < cacheExpiry) {
@@ -33,7 +63,7 @@ async function loadJisyoFromUri(memento: vscode.Memento, uri: Uri): Promise<Jisy
     }
 
     // clear cache
-    await memento.update(cacheKey, undefined);
+    await memento.update(systemJisyoKey, undefined);
     await memento.update(cacheExpiryKey, undefined);
 
     // download jisyo from uri
@@ -44,7 +74,7 @@ async function loadJisyoFromUri(memento: vscode.Memento, uri: Uri): Promise<Jisy
     const rawJisyo = Buffer.from(await response.arrayBuffer());
 
     const jisyo = rawSKKJisyoToJisyo(rawJisyo);
-    await memento.update(cacheKey, Object.fromEntries(jisyo));
+    await memento.update(systemJisyoKey, Object.fromEntries(jisyo));
     await memento.update(cacheExpiryKey, now + 1000 * 60 * 60 * 24 * 30); // 30 days
     return jisyo;
 }
@@ -84,143 +114,4 @@ function rawSKKJisyoToJisyo(rawLines: Buffer): Jisyo {
         }
     }
     return jisyo;
-}
-
-class CompositeMap<K, V> implements Map<K, V> {
-    private maps: Map<K, V>[] = [];
-
-    constructor(maps: Map<K, V>[]) {
-        assert(maps.length > 0);
-        this.maps = maps;
-    }
-
-    /**
-     * Return the value from the first map that has the key.
-     * @param key 
-     * @returns 
-     */
-    get(key: K): V | undefined {
-        for (const map of this.maps) {
-            if (map.has(key)) {
-                return map.get(key);
-            }
-        }
-        return undefined;
-    }
-
-    /**
-     * Set the value for the given key in the first map.
-     * @param key 
-     * @param value 
-     * @returns 
-     */
-    set(key: K, value: V): this {
-        this.maps[0].set(key, value);
-        return this;
-    }
-
-    /**
-     * Does not support this operation.
-     * @param key 
-     * @returns 
-     */
-    clear(): void {
-        throw new Error("Method not implemented.");
-    }
-    /**
-     * Does not support this operation.
-     * @param key 
-     * @returns 
-     */
-    delete(key: K): boolean {
-        throw new Error("Method not implemented.");
-    }
-
-    /**
-     * Delete the key from the first map.
-     */
-    deleteFromFirst(key: K): boolean {
-        return this.maps[0].delete(key);
-    }
-
-    /**
-     * Iterates over the all pairs of key and value.
-     * Only the first pair in the maps is processed.
-     * @param callbackfn 
-     * @param thisArg 
-     * @returns 
-     */
-    forEach(callbackfn: (value: V, key: K, map: Map<K, V>) => void, thisArg?: any): void {
-        for (let entry of this.entries()) {
-            // treat thisArg as "this" in callbackfn
-            if (thisArg === undefined) {
-                callbackfn(entry[1], entry[0], this);
-            } else {
-                callbackfn.apply(thisArg, [entry[1], entry[0], this]);
-            }
-        }
-    }
-
-    /**
-     * Checks if the key is in the maps.
-     * @param key 
-     */
-    has(key: K): boolean {
-        for (const map of this.maps) {
-            if (map.has(key)) {
-                return true;
-            }
-        }
-        return false;
-    }
-
-    /**
-     * Returns iterator over the all pairs of key and value.
-     */
-    *entries(): IterableIterator<[K, V]> {
-        let seen = new Set<K>();
-        for (const map of this.maps) {
-            for (const [key, value] of map.entries()) {
-                if (!seen.has(key)) {
-                    yield [key, value];
-                    seen.add(key);
-                }
-            }
-        }
-    }
-
-    *keys(): IterableIterator<K> {
-        for (let entry of this.entries()) {
-            yield entry[0];
-        }
-    }
-
-    *values(): IterableIterator<V> {
-        for (let entry of this.entries()) {
-            yield entry[1];
-        }
-    }
-
-    [Symbol.iterator](): IterableIterator<[K, V]> {
-        return this.entries();
-    }
-    
-    get size(): number {
-        let seen = new Set<K>();
-        for (const map of this.maps) {
-            for (const key of map.keys()) {
-                if (!seen.has(key)) {
-                    seen.add(key);
-                }
-            }
-        }
-        return seen.size;
-    }
-
-    /**
-     * Returns "[object MultiMap]".
-     */
-    get [Symbol.toStringTag](): string {
-        return "[object MultiMap]";
-    }
 }
