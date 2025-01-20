ac5f346187e3a30ac5707d9bd50fbc455e96fed2
Yasuhiro Hayase <hayase@cs.tsukuba.ac.jp>
2025-01-20 21:55:49 +0900

    Add tests for okurigana and existing midashigo

--- src/tests/integration/suite/henkan/registration-feature.test.ts
+++ src/tests/integration/suite/henkan/registration-feature.test.ts
@@ -4,6 +4,8 @@ import { getGlobalJisyo } from '../../../../jisyo/jisyo';
 
 suite('Registration feature test', async () => {
     const unexistYomi = 'りですごじめわゅょぼざゔにろせふよふ';
+    const okuriganaAlphabet = 'Sa';
+    const okuriganaAlphabetPrefix = 's';
 
     setup('新しい空のエディタを開き、見出し語が登録されていない状態にする', async () => {
         await vscode.commands.executeCommand('workbench.action.files.newUntitledFile');
@@ -11,6 +13,7 @@ suite('Registration feature test', async () => {
         await vscode.commands.executeCommand('skk.nop');
         const globalJisyo = getGlobalJisyo();
         globalJisyo?.delete(unexistYomi);
+        globalJisyo?.delete(unexistYomi+okuriganaAlphabetPrefix);
     });
 
     teardown('エディタを閉じ、登録された不要な見出し語を削除する', async () => {
@@ -19,9 +22,10 @@ suite('Registration feature test', async () => {
         await vscode.commands.executeCommand('skk.nop');
         const globalJisyo = getGlobalJisyo();
         globalJisyo?.delete(unexistYomi);
+        globalJisyo?.delete(unexistYomi+okuriganaAlphabetPrefix);
     });
 
-    test('存在しない見出し語を変換しようとすると、辞書登録エディタが開く', async () => {
+    test('存在しない送りなし見出し語を変換しようとすると、辞書登録エディタが開く', async () => {
         const document = vscode.window.activeTextEditor?.document;
         assert.notEqual(document, undefined);
 
@@ -50,6 +54,82 @@ suite('Registration feature test', async () => {
         });
     });
 
+    test('存在しない送りあり見出し語を変換しようとすると、辞書登録エディタが開く', async () => {
+        const document = vscode.window.activeTextEditor?.document;
+        assert.notEqual(document, undefined);
+
+        // ひらがなモードに切り替える
+        await vscode.commands.executeCommand('skk.ctrlJInput');
+
+        // Q キーを入力して、見出し語モードに切り替える
+        await vscode.commands.executeCommand('skk.upperAlphabetInput', 'Q');
+
+        // エディタに，辞書に存在しない語の読みを入力する
+        await vscode.commands.executeCommand('type', { text: unexistYomi });
+        for (const c of okuriganaAlphabet) {
+            if (c === c.toUpperCase()) {
+                await vscode.commands.executeCommand('skk.upperAlphabetInput', c);
+            } else {
+                await vscode.commands.executeCommand('skk.lowerAlphabetInput', c);
+            }
+        }
+
+        // スペースキーを入力して、見出し語の変換を試みる
+        await vscode.commands.executeCommand('skk.spaceInput');
+
+        // 新しいエディタが開かれ、内容が辞書登録の初期コンテンツであることを確認する
+        return new Promise(resolve => {
+            const disposable = vscode.workspace.onDidChangeTextDocument(async e => {
+                if (e.document !== document) {
+                    disposable.dispose();
+                    assert.equal(e.document.getText(), `読み:${unexistYomi+okuriganaAlphabetPrefix}\n単語:`);
+                    await vscode.commands.executeCommand('workbench.action.closeActiveEditor');
+                    resolve();
+                }
+            });
+        });
+    });
+
+    test('存在する見出し語候補の最後まで到達すると、辞書登録エディタが開く', async () => {
+        const document = vscode.window.activeTextEditor?.document;
+        assert.notEqual(document, undefined);
+
+        // unexistYomi に対応する候補を1つ登録する
+        const existWord = '候補1';
+        getGlobalJisyo().set(unexistYomi, [{ word: existWord, annotation: undefined }]);
+
+        // ひらがなモードに切り替える
+        await vscode.commands.executeCommand('skk.ctrlJInput');
+
+        // Q キーを入力して、見出し語モードに切り替える
+        await vscode.commands.executeCommand('skk.upperAlphabetInput', 'Q');
+
+        // エディタに，辞書に存在しない語の読みを入力する
+        await vscode.commands.executeCommand('type', { text: unexistYomi });
+
+        // スペースキーを入力して、見出し語の変換を開始する
+        await vscode.commands.executeCommand('skk.spaceInput');
+
+        const disposable1 = await vscode.workspace.onDidChangeTextDocument(async e => {
+            disposable1.dispose();
+            // 1つ目の候補が出ている状態で、さらにスペースキーを入力する
+            await vscode.commands.executeCommand('skk.spaceInput');
+        });
+        
+        // 新しいエディタが開かれ、内容が辞書登録の初期コンテンツであることを確認する
+        return new Promise(resolve => {
+            const disposable = vscode.workspace.onDidChangeTextDocument(async e => {
+                if (e.document !== document) {
+                    disposable.dispose();
+                    assert.equal(e.document.getText(), `読み:${unexistYomi}\n単語:`);
+                    await vscode.commands.executeCommand('workbench.action.closeActiveEditor');
+                    resolve();
+                }
+            });
+        });
+    });
+
+
     test('辞書登録エディタで見出し語を登録すると、ユーザ辞書に見出し語が登録される', async () => {
         const unexistWord = 'かおたへちぶぬもほゃぢめろめちゅのめ';
 
@@ -69,4 +149,5 @@ suite('Registration feature test', async () => {
         assert.equal(candidate?.[0].word, unexistWord);
         assert.equal(candidate?.[0].annotation, undefined);
     });
+
 });
